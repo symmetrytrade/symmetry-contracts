4bad3192e6cda5dd0e2578306832d92b2e1bb56a
ringleo <leo@symmetry.trade>
2023-05-12 16:35:17 +0800

    fix: leverage ratio check

--- contracts/manager/PositionManager.sol
+++ contracts/manager/PositionManager.sol
@@ -280,11 +280,11 @@ contract PositionManager is MarketSettingsContext, Ownable, Initializable {
             // do trade
             market_.trade(order.account, order.token, order.size, fillPrice);
         }
-        // ensure leverage ratio is higher than max laverage ratio, or is position decrement
+        // ensure leverage ratio is lower than max laverage ratio, or is position decrement
         require(
-            !leverageRatioExceeded(order.account) ||
-                (prevSize < 0 && order.size > 0 && prevSize + order.size < 0) ||
-                (prevSize > 0 && order.size < 0 && prevSize + order.size > 0),
+            (prevSize < 0 && order.size > 0 && prevSize + order.size <= 0) ||
+                (prevSize > 0 && order.size < 0 && prevSize + order.size >= 0) ||
+                !leverageRatioExceeded(order.account),
             "PositionManager: leverage ratio too large"
         );
         // update token market info
