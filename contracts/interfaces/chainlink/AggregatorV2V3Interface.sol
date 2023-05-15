e0e2ca29b35c8266e6e14a3fd91c8c16f6b894f9
ringleo <leo@symmetry.trade>
2023-05-15 17:43:44 +0800

    chore: chainlink interfaces

--- contracts/interfaces/chainlink/AggregatorV2V3Interface.sol
+++ contracts/interfaces/chainlink/AggregatorV2V3Interface.sol
@@ -1,29 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface AggregatorV2V3Interface {
-    function latestAnswer() external view returns (int);
+import "./AggregatorInterface.sol";
+import "./AggregatorV3Interface.sol";
 
-    function latestTimestamp() external view returns (uint);
-
-    function latestRound() external view returns (uint);
-
-    function getAnswer(uint roundId) external view returns (int);
-
-    function getTimestamp(uint roundId) external view returns (uint);
-
-    function decimals() external view returns (uint8);
-
-    function description() external view returns (string memory);
-
-    function version() external view returns (uint);
-
-    function getRoundData(
-        uint80 _roundId
-    ) external view returns (uint80 roundId, int answer, uint startedAt, uint updatedAt, uint80 answeredInRound);
-
-    function latestRoundData()
-        external
-        view
-        returns (uint80 roundId, int answer, uint startedAt, uint updatedAt, uint80 answeredInRound);
-}
+interface AggregatorV2V3Interface is AggregatorInterface, AggregatorV3Interface {}
